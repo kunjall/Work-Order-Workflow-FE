7fa0ee81843240b165f94ba237ff1eb5467c7740
Kunjal <kunjal@Kunjals-MacBook-Air.local>
2025-08-11 22:16:50 +0530

    vendor route allocation

--- src/components/createFiberWorkorder/child/Child.js
+++ src/components/createFiberWorkorder/child/Child.js
@@ -511,6 +511,14 @@ const DashboardWhinch = () => {
       !childWorkOrderNumber ||
       !selectedManager
     ) {
+      console.log("Missing fields:");
+      if (!selectedApproverEmail)
+        console.log(" - selectedApproverEmail is missing");
+      if (!selectedWorkOrder) console.log(" - selectedWorkOrder is missing");
+      if (!childWorkOrderNumber)
+        console.log(" - childWorkOrderNumber is missing");
+      if (!selectedManager) console.log(" - selectedManager is missing");
+
       window.alert("Please select all fields before proceeding.");
       return;
     }
